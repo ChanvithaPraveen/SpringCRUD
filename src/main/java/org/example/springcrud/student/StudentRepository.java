822878a30a0e8ce9d7f513af9428df4501b9fbb2
Chanvitha Praveen <chanvithapraween@gmail.com>
2024-03-08 20:52:54 +0530

    created interfaces and repository

---
+++ src/main/java/org/example/springcrud/student/StudentRepository.java
@@ -0,0 +1,13 @@
+package org.example.springcrud.student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
--- src/main/java/org/example/springcrud/student/StudentService.java
+++ src/main/java/org/example/springcrud/student/StudentService.java
@@ -1,5 +1,6 @@
 package org.example.springcrud.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +11,14 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(new Student(1L, "James", "Smith",LocalDate.of(1998, Month.APRIL, 17), 26));
+        return studentRepository.findAll();
     }
 }
